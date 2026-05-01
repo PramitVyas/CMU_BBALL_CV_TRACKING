07a5737026bd32b989bb74b801cb51d81708f42d
Alex Hill <ajhill@ghidorah1.stat.cmu.edu>
2026-05-01 22:14:10 +0000

    empty experimental branch

--- project/constants.py
+++
@@ -1,4 +0,0 @@
-TEAM_COLORS = {
-    "Carnegie Mellon University": "#FF0000",
-    "Road Team": "#0000FF"
-}
--- project/paths.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-# Project root
-HOME = Path(__file__).resolve().parent.parent
-
-# Standard subdirectories
-SOURCE_VIDEO_DIRECTORY = HOME / "source"
-CHECKPOINTS_DIRECTORY = HOME / "checkpoints"
-OUTPUT_DIRECTORY = HOME / "output"
